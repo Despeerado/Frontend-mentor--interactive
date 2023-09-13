fffc90023007409fb410316d523a892699d23425
Dominik Beran <d.beran27@gmail.com>
2023-09-13 21:42:28 +0200

    fonts and colors settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      'lightGray': '#dedddf',
+      'darkGray': '#8e8593',
+      'darkViolet': '#21092f',
+    },
     extend: {},
   },
   plugins: [],
