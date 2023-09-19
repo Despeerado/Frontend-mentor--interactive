f629fd554b00a447ecc9e69d3213cd93eab94791
Dominik Beran <d.beran27@gmail.com>
2023-09-20 00:27:47 +0200

    padding - modal

--- script.js
+++ script.js
@@ -20,56 +20,56 @@ const errorCard = document.getElementById('errorCard');
 const errorDate = document.getElementById('errorDate');
 const errorCVC = document.getElementById('errorCvc');
 
+// spans
+
+const spanNumber = document.getElementById('spanNumber')
+const spanName = document.getElementById('spanName')
+const spanMonth = document.getElementById('spanMonth')
+const spanYear = document.getElementById('spanYear')
+const spanCVC = document.getElementById('spanCVC')
+
 let isValidCardName = false;
 let isValidCardNumber = false;
 let isValidCardMonth = false;
 let isValidCardYear = false;
 let isValidCardCVC = false;
 
-	// function validation() {
-	// if (isValidCardName && isValidCardNumber && isValidCardMonth && isValidCardYear && isValidCardCVC) {
-	// 	alert("Done")
-	// }
-	// else {
-	// 	alert("Ne ne")
-	// }
-	// }
-
 const submit = document.getElementById('submit');
 submit.addEventListener('click', validate);
 
-function validate(e) {
-	e.preventDefault();
-	let valid = true;
+// function validate(e) {
+// 	e.preventDefault();
+// 	let valid = true;
 
-	if (isValidCardName && isValidCardNumber && isValidCardMonth && isValidCardYear && isValidCardCVC) {
-		document.getElementById('modal').classList.remove('invisible');
-		document.getElementsByClassName('contact-form')[0].classList.add('invisible');
-	}
-	return valid;
-}
+// 	if (isValidCardName && isValidCardNumber && isValidCardMonth && isValidCardYear && isValidCardCVC) {
+// 		document.getElementById('modal').classList.remove('invisible');
+// 		document.getElementsByClassName('contact-form')[0].classList.add('invisible');
+// 	}
+// 	return valid;
+// }
 
 ready(() => {
 
 	const events = ['focus', 'keyup', 'keypress'];
 
-new Cleave('#cardNumber', {
-	creditCard: true,
-	delimiter: ' ',
-	numericOnly: true,
-});
+	new Cleave('#cardNumber', {
+		creditCard: true,
+		delimiter: ' ',
+		numericOnly: true,
+		blocks: [4, 4, 4, 4]
+	});
 
-new Cleave('#month', {
-    date: true,
+	new Cleave('#month', {
+		date: true,
 		datePattern: ['m'],
 		numericOnly: true,
-});
+	});
 
-new Cleave('#year', {
-    date: true,
+	new Cleave('#year', {
+		date: true,
 		datePattern: ['y'],
 		numericOnly: true,
-});
+	});
 
 	new Cleave('#cvc', {
 		numericOnly: true,
@@ -92,6 +92,7 @@ new Cleave('#year', {
 
 			cardName.classList.remove('border-red');
 			errorName.classList.add('error');
+			spanName.innerHTML = cardName.value;
 			isValidCardName = true;
 		});
 
@@ -102,44 +103,51 @@ new Cleave('#year', {
 				cardNumber.classList.add('border-red');
 				errorCard.classList.remove('error');
 				isValidCardNumber = false;
-				return;	
+				return;
 			}
 
 			cardNumber.classList.remove('border-red');
 			errorCard.classList.add('error');
 			isValidCardNumber = true;
+			spanNumber.innerHTML = cardNumber.value;
 			return;
 		
 		});
 		// validation - month
 		cardMonth.addEventListener(evt, () => {
-			if (cardMonth.value == undefined || cardMonth.value.trim().length <= 0)
-			{
+			if (cardMonth.value == undefined || cardMonth.value.trim().length <= 1) {
+				spanMonth.innerHTML = '00';
 				cardMonth.classList.add('border-red');
 				errorDate.classList.remove('error');
 				isValidCardMonth = false;
 				return;
 			}
-				cardMonth.classList.remove('border-red');
-				errorCard.classList.add('error');
-				isValidCardMonth = true;
-				return;
+			date = cardMonth.value;
+			spanMonth.innerHTML = date;
+			cardMonth.classList.remove('border-red');
+			errorCard.classList.add('error');
+			isValidCardMonth = true;
+	
+			return;
 		});
 
 		// validation - year
 
 		cardYear.addEventListener(evt, () => {
-			if (cardYear.value == undefined || cardMonth.value.trim().length <= 0)
-			{
+			if (cardYear.value == undefined || cardYear.value.trim().length <= 1) {
+				spanYear.innerHTML = '00';
 				cardYear.classList.add('border-red');
 				errorDate.classList.remove('error');
 				isValidCardYear = false;
 				return;
 			}
-				cardYear.classList.remove('border-red');
-				errorDate.classList.add('error');
-				isValidCardYear = true;
-				return;
+			date = cardYear.value;
+			spanYear.innerHTML = date;
+			cardYear.classList.remove('border-red');
+			errorDate.classList.add('error');
+			isValidCardYear = true;
+
+			return;
 		});
 		// });
 		
@@ -147,10 +155,13 @@ new Cleave('#year', {
 		// validation - cvc
 		cardCVC.addEventListener(evt, () => {
 
+			spanCVC.innerHTML = cardCVC.value;
+
 			if (cardCVC.value == undefined || cardCVC.value.trim().length <= 0) {
 				cardCVC.classList.add('border-red');
 				errorCVC.innerHTML = "Can't be blank";
 				errorCVC.classList.remove('error');
+				spanCVC.innerHTML = '000';
 				isValidCardCVC = false;
 				return;
 			}
@@ -167,8 +178,31 @@ new Cleave('#year', {
 				isValidCardCVC = true;
 				return;
 			}
-		})
+		});
+
+
+
 	});
 
 });
+
+function validate() {
+	
+	if (
+		isValidCardName &&
+		isValidCardNumber &&
+		isValidCardMonth &&
+		isValidCardYear &&
+		isValidCardCVC
+) {
+//HIDE FORM AND SHOW MODAL CONFIRM
+	document.getElementById('modal').classList.remove('hidden');
+	document.getElementById('cardForm').classList.add('hidden');
+	}
+}
+
+function reset() {
+		document.getElementById('modal').classList.add('hidden');
+	document.getElementById('cardForm').classList.remove('hidden');
+}
 
